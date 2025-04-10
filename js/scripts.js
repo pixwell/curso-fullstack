13dfd9bfa9dab0a5963c78ee3c39abbde7b081eb
Marcia Silva <contato.pix@gmail.com>
2025-04-10 14:44:17 -0300

    Programação inicial

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('add-item');
+const item_field = document.getElementById('item-name');
+const list_container = document.getElementById('items-list');
+const status_container = document.getElementById('status');
+const alertError_container = document.getElementById('alert-error');
+const alertSuccess_container = document.getElementById('alert-success');

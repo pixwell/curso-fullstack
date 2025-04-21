d8640d42c3337c3e61f6342494a6e561b2bbf3c2
Marcia Silva <contato.pix@gmail.com>
2025-04-21 16:13:20 -0300

    Programação da lista de números sorteados

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+const form = document.getElementById('form-sorteio');
+const numQtd = document.getElementById('num-qtd');
+const numFrom = document.getElementById('num-de');
+const numTo = document.getElementById('num-ate');
+const numRepetir = document.getElementById('num-repetir');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  let lista = numerosSorteados();
+
+  console.log(lista);
+});
+
+/**
+ * Lista de numeros sorteados
+ * @returns array
+ */
+function numerosSorteados(){
+    let ArrayRandomNumber = [];
+  
+    //Sortear a quantidade de número dentro do range
+    for (let i = 0; i < numQtd.value; i++) {
+        let randomNumber = numeroRandomico(numFrom.value, numTo.value);
+
+        if(numRepetir.checked){
+            while (ArrayRandomNumber.includes(randomNumber)) { 
+                randomNumber = numeroRandomico(numFrom.value, numTo.value);
+            }
+        }
+
+        ArrayRandomNumber.push(randomNumber);
+    }
+
+    //Retorna um array em ordem crescente
+    return ArrayRandomNumber.sort((a, b) => (a - b));
+}
+
+/**
+ * Sorteia um numero dentro do range
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns Number
+ */
+function numeroRandomico(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}

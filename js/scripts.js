7349589cdc54cfcd4b05dbf89ff8c0ade60e4a1b
Marcia Silva <contato.pix@gmail.com>
2025-04-24 13:41:37 -0300

    Refatoração da lógica utilizando Eventos Customizados e Promises

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,149 @@ const numQtd = document.getElementById('num-qtd');
 const numFrom = document.getElementById('num-de');
 const numTo = document.getElementById('num-ate');
 const numRepetir = document.getElementById('num-repetir');
+const sorteioContainer = document.getElementById('sorteio-container');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  let lista = numerosSorteados();
+if (form) {
+    form.addEventListener('submit', async function(event) {
+        event.preventDefault();
 
-  console.log(lista);
-});
+        //Carrega a secao que vai conter os numeros
+        carregaContainerSorteio();
+        //Carrega a lista de numeros sorteados
+        await montaListaSorteada();
+    });    
+}
+
+/**
+ * Adicina os numeros sorteador na
+ */
+async function montaListaSorteada(){    
+    //Espera carregar o container principal
+    await esperarEvento('containerSorteio_carregado');
+
+    //Carrega cada numero
+    const lista = arrNumerosSorteados();
+
+    //Adiciona os numeros
+    for (const item of lista) {
+        //adiciona o numero
+        addNumerosNaLista(item);
+        //Espera ele avisar que o ciclo de animacao acabou (evento)
+        await esperarEvento('addNumero_carregado');
+    }
+
+    // === Dispara evento quando terminar ===
+    let eventoTerminado = new CustomEvent('numerosSorteados_carregado');
+    document.dispatchEvent(eventoTerminado);
+}
+
+/**
+ * Transforma eventos em Promises
+ * @param {string} nomeEvento 
+ * @returns Object
+ */
+function esperarEvento(nomeEvento) {
+    return new Promise(resolve => {
+        const handler = () => {
+            document.removeEventListener(nomeEvento, handler);
+            resolve();
+        };
+        document.addEventListener(nomeEvento, handler);
+    });
+}
+
+/**
+ * Ciclo de animacoes para adicionar o numero
+ * @param {Number} num 
+ */
+function addNumerosNaLista(num){
+    //Animacao
+    let apresentacao = document.getElementById('apresentacao');
+
+    apresentacao.textContent = num;
+    apresentacao.classList.add('zoomInDown');
+    apresentacao.classList.remove('d-none');
+
+    setTimeout(() => {
+        apresentacao.classList.add('fade-out-up');
+        apresentacao.classList.remove('zoomInDown');
+    }, 2000);
+
+    //Lista de numeros
+    let ul = document.getElementById('resultado-sorteio');
+
+    let li = document.createElement('li');
+    li.textContent = num;
+    
+    setTimeout(() => {
+        li.classList.add('fade-in');
+        ul.appendChild(li);
+    }, 2200);
+
+    setTimeout(() => {
+        //Reseta as classes CSS
+        apresentacao.classList.remove('fade-out-up');
+        apresentacao.classList.add('d-none');
+        li.classList.remove('fade-in');
+
+        // === Dispara evento quando o numero carregar ===
+        let eventoTerminado = new CustomEvent('addNumero_carregado');
+        document.dispatchEvent(eventoTerminado);
+    }, 2250);
+}
+
+/**
+ * Carrega a estrutura inicial para carregar os numeros sorteados
+ */
+function carregaContainerSorteio(){
+    //Limpa o container
+    sorteioContainer.classList.add('fade-out');
+    //Da o tempo para a animacao terminar
+    setTimeout(() => sorteioContainer.innerHTML = '', 800);
+
+    //<h3 class="label-large mb-0 text-center">Resultado do sorteio</h3>
+    let titulo = document.createElement('h3');
+    titulo.classList.add('label-large','mb-0', 'text-center');
+    titulo.textContent = 'Resultado do sorteio';
+
+    //<h4 class="title-overline text-center" id="subtitulo"><span class="gradient-gray-text"><span>1</span>° resultado</span></h4>
+    let subTitulo = document.createElement('h4');
+    subTitulo.classList.add('title-overline', 'text-center');    
+    subTitulo.setAttribute('id','subtitulo');
+    subTitulo.innerHTML = '<span class="gradient-gray-text"><span>1</span>° resultado</span>';
+
+    //<ul id="resultado-sorteio" class="resultado-sorteio">
+    let ul = document.createElement('ul');
+    ul.classList.add('resultado-sorteio');    
+    ul.setAttribute('id','resultado-sorteio');
+
+    //<div id="apresentacao" class="apresentacao d-none"></div>
+    let apresentacao = document.createElement('div');
+    apresentacao.classList.add('apresentacao', 'd-none');
+    apresentacao.setAttribute('id','apresentacao');
+
+    // === Carrega os containers na tela
+    setTimeout(() => {
+        sorteioContainer.appendChild(titulo);
+        sorteioContainer.appendChild(subTitulo);
+        sorteioContainer.appendChild(ul);
+        sorteioContainer.appendChild(apresentacao);
+        sorteioContainer.classList.remove('fade-out','fade-in');
+        sorteioContainer.classList.add('fade-in');
+    }, 900);
+
+    // === Dispara evento quando container carregar ===
+    setTimeout(() => {
+        let eventoTerminado = new CustomEvent('containerSorteio_carregado');
+        document.dispatchEvent(eventoTerminado);
+    }, 950);
+}
 
 /**
  * Lista de numeros sorteados
  * @returns array
  */
-function numerosSorteados(){
+function arrNumerosSorteados(){
     let ArrayRandomNumber = [];
   
     //Sortear a quantidade de número dentro do range

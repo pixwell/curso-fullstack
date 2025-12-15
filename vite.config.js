3f07fe355bfe8092e00e9c4a7dcd1bb3a98891cd
Marcia Silva <contato.pix@gmail.com>
2025-12-15 17:08:54 -0300

    instalação do Tailwind e suas dependências

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   root: '.',
   server: {
     port: 5173
